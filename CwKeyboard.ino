5bc53b2f80510e5f23d1dac094c88e43f1260922
George Mathews <george@georgemathe.ws>
2020-09-25 03:44:13 -0700

    Started tracking elements for full keyboard

--- CwKeyboard.ino
+++ CwKeyboard.ino
@@ -1,22 +1,35 @@
 // Constants
-const int kModePin = 0;
-const int kKeyPin = 1;
-const int kLedPin = 2;
+const byte kModePin = 0;
+const byte kKeyPin = 1;
+const byte kLedPin = 2;
 // Configuration constants
-const int kMinModeSwitchPressCycles = 10;
+const byte kMinModeSwitchPressCycles = 10;
 const int kSampleHz = 100;
-const int kMaxTimerValue = 65535;
+const unsigned short kMaxTimerValue = 65535;
+// Define length of elements
+const byte kLengthDot = 1;
+const byte kLengthDash = 3;
+const byte kLengthGapElement = 1;
+const byte kLengthGapLetter = 3;
+const byte kLengthGapWord = 7;
 
 // State variables
 bool fullKeyboardMode;
 unsigned short modeTimer;
 unsigned short keyPressedTimer;
 unsigned short keyReleasedTimer;
+unsigned short averageElementSize;
+bool allowSpaceInserted;
+// The size of each element
+byte elements[8];
 
 void resetStateOnModeSwitch() {
     modeTimer = 0;
     keyPressedTimer = 0;
     keyReleasedTimer = 1; // Make sure we don't trigger key released logic
+    allowSpaceInserted = false;
+    clearElements(elements);
+    averageElementSize = 24; // Hard code for now
     // Make sure we don't get stuck
     Keyboard.releaseAll();
 }
@@ -65,6 +78,10 @@ void fullKeyboardUpdate() {
         if (keyPressedTimer == 0) {
             // Visual indicator
             digitalWrite(kLedPin, HIGH);
+            // If the last thing printed wasn't a prosign, add a space
+            if(allowSpaceInserted && keyReleasedTimer > kLengthGapWord * averageElementSize){
+                Keyboard.print(' ');
+            }
             keyReleasedTimer = 0;
         }
         updateTimer(&keyPressedTimer);
@@ -73,10 +90,16 @@ void fullKeyboardUpdate() {
         if (keyReleasedTimer == 0) {
             // Turn off visual indicator
             digitalWrite(kLedPin, LOW);
-            keyPressedTimer = 0;
 
-            // TODO: Determine which key to send
-            char key = 'a';
+            // Update our buffer with the element we made
+            updateElements(elements, keyPressedTimer);
+            // Reset relevant data
+            keyPressedTimer = 0;
+        }
+
+        if (keyReleasedTimer > kLengthGapLetter * averageElementSize){
+            char key = getLetter(elements);
+            allowSpaceInserted = isControl(key);
             Keyboard.print(key);
 
             // Debug what happened
@@ -84,6 +107,14 @@ void fullKeyboardUpdate() {
         }
         updateTimer(&keyReleasedTimer);
     }
+}
+
+void clearElements(byte elements[]){
+}
+void updateElements(byte elements[], unsigned short keyPressTime){
+}
+char getLetter(byte elements[]){
+    return 'a';
 }
 
 void practiceKeyboardUpdate() {
@@ -105,6 +136,7 @@ void indicateMode() {
     digitalWrite(kLedPin, !fullKeyboardMode);
 }
 
+// Make sure we don't wrap around
 void updateTimer(unsigned short *timer) {
     if (*timer < kMaxTimerValue) {
         (*timer)++;
